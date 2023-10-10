9081cf83a82ae5eb52eae262eae00353459398e5
alextor26 <alextor2625@gmail.com>
2023-10-10 18:03:51 -0400

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,72 @@
 import "./App.css";
+import data from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  // const [contacts, setContact] = useState(contacts)
+  const [contacts, setContact] = useState(data.slice(0, 5));
+
+  const deleteContact = (id) => {
+    let deleted = contacts.filter((elem) => elem.id)
+  }
+
+  // const sortByPopularity
+  const sortByPopularity = () => {
+    let sorted = [...contacts].sort((a,b) => b.popularity - a.popularity)
+    setContact(sorted)
+  }
+  const sortByName = () => {
+    let sorted = [...contacts].sort((a,b) => b.name.localeCompare(a.name))
+    setContact(sorted)
+  }
+  const addRandomContact = () => {
+    let remaining = data.filter((elem) => !contacts.includes(elem));
+    console.log(remaining.length === data.length);
+    if (!(remaining.length === data.length)) {
+      let randomIndex = Math.floor(Math.random() * remaining.length);
+      let newArr = [remaining[randomIndex], ...contacts];
+      console.log(newArr.length);
+      setContact([remaining[randomIndex], ...contacts]);
+     } 
+     else {
+      setContact(contacts);
+    }
+  };
+
   return (
     <div className="App">
-      <h1>LAB | React IronContacts</h1>
+      <h1>Iron Contacts</h1>
+
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by Popularity</button>
+      <button onClick={sortByName}>Sort by Name</button>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+              <tr>
+                <td>
+                  <img src={contact.pictureUrl} />
+                </td>
+                <td><p>{contact.name}</p></td>
+                <td><p>{contact.popularity}</p></td>
+                <td>{contact.wonOscar && <p>🏆</p>}</td>
+                <td>{contact.wonEmmy && <p>🏆</p>}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
